3e361387ea97ffb25ffe0459b70836bc9fddba2d
Tomás Lopes <33103241+tomlopes@users.noreply.github.com>
2019-10-15 21:47:12 +0100

    add Fence

--- js/Fence.js
+++ js/Fence.js
@@ -1,21 +1,37 @@
-export default class Fence {
-	object = null;
+import './three.js';
+('use strict');
+
+export default class Fence extends THREE.Object3D {
 	material = null;
 
 	constructor(x, y, z) {
-		this.object = new THREE.Object3D();
+		super();
 		this.material = new THREE.MeshBasicMaterial({
-			color: 0x00ff00,
+			color: 0xff0000,
 			wireframe: false,
 		});
 
-		this.object.position.set(x, y, z);
+		this.addWall(0, 0, 0);
+		this.addWall(0, 0, 0);
+		this.addWall(0, 0, 0);
+		this.moveWalls();
+
+		this.position.set(x, y + 10, z);
 	}
 
-	createWall(x, y, z) {
-		let geometry = new THREE.CubeGeometry(10, 4, 20);
-		let mesh = new THREE.Mesh(geometry, this.material);
-		mesh.position.set(x, y, z);
-		this.object.add(mesh);
+	addWall(x, y, z) {
+		let geometry = new THREE.CubeGeometry(100, 4, 20);
+		let wall = new THREE.Mesh(geometry, this.material);
+		wall.position.set(x, y, z);
+		this.add(wall);
+	}
+
+	moveWalls() {
+		this.children[0].rotateX(Math.PI / 2); // puts the wall vertical
+		this.children[0].rotateZ(Math.PI / 2); // puts the back wall perpendicular to the others
+		this.children[1].rotateX(Math.PI / 2);
+		this.children[1].position.set(48, 0, -50);
+		this.children[2].rotateX(Math.PI / 2);
+		this.children[2].position.set(48, 0, 50);
 	}
 }
